a382b69fd449d7ee02ab0d33ba8eb9399f85d043
tianxl <1823871721@qq.com>
2019-07-03 16:15:21 +0800

    feat(user): 用户列表页完成

--- src/views/Function/Users.vue
+++ src/views/Function/Users.vue
@@ -5,30 +5,67 @@
       title="用户列表"
     >
       <div class="emq-table-header">
-        <el-button type="primary" size="small" icon="el-icon-plus" @click="createUsers">创 建</el-button>
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-plus"
+          @click="showDialog('create')">创 建</el-button>
       </div>
 
       <el-table :data="tableData" class="data-list">
-        <el-table-column label="跟老的一致" prop="username"></el-table-column>
+        <el-table-column label="用户名" prop="username"></el-table-column>
+        <el-table-column label="备注" prop="tags"></el-table-column>
+        <el-table-column>
+          <template slot-scope="{ row }">
+            <el-button type="dashed" size="mini" @click="showDialog('edit', row)">编辑</el-button>
+            <el-button
+              v-if="row.tags !== 'administrator'"
+              type="dashed"
+              size="mini"
+              @click="deleteConfirm(row)">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
 
     </a-card>
 
 
-    <el-dialog title="创建用户 或者 编辑用户 你自己维护状态" width="520px" :visible.sync="dialogVisible">
-      <el-form :model="record" :rules="rules" size="small">
-        <el-row :gutter="40">
-          <el-col :span="12">
-            <el-form-item prop="name" label="用户名称">
-              <el-input v-model="record.name"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
+    <el-dialog
+      width="520px"
+      :title="accessType === 'edit' ? '编辑用户' : '创建用户'" 
+      :visible.sync="dialogVisible">
+      <el-form size="small" ref="recordForm" :model="record" :rules="rules">
+        <el-form-item prop="username" label="用户名称" >
+          <el-input
+            v-model="record.username"
+            :disabled="accessType === 'edit'"></el-input>
+        </el-form-item>
+        <el-form-item prop="tags" label="备注">
+          <el-input v-model="record.tags"></el-input>
+        </el-form-item>
+          <el-form-item
+            v-if="accessType !== 'edit' || allowChange"
+            prop="password"
+            :label="accessType === 'edit' ? '旧密码' : '密码'">
+            <el-input v-model="record.password"></el-input>
+          </el-form-item>
+          <el-form-item v-if="allowChange" prop="newPassword" label="新密码">
+            <el-input v-model="record.newPassword"></el-input>
+          </el-form-item>
+          <el-form-item v-if="allowChange" prop="repeatPassword" label="确认新密码">
+            <el-input v-model="record.repeatPassword"></el-input>
+          </el-form-item>
+        <el-link
+          v-if="accessType === 'edit'"
+          :underline="false"
+          @click="togglePassword">
+          {{ allowChange ? '不修改密码' : '修改密码' }}
+        </el-link>
       </el-form>
 
       <div slot="footer" class="dialog-align-footer">
-        <el-button type="primary" size="small">确 定</el-button>
-        <el-button plain size="small">取 消</el-button>
+        <el-button type="primary" size="small" @click="save">确定</el-button>
+        <el-button plain size="small" @click="closeDialog">取消</el-button>
       </div>
     </el-dialog>
   </div>
@@ -53,25 +90,52 @@ export default {
   props: {},
 
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.record.newPassword) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    }
     return {
       dialogVisible: false,
       tableData: [],
-      record: {
-        newPassword: '',
-        password: '123321',
-        repeatPassword: '',
-        tags: 'viewer',
-        username: 'wivwiv',
-      },
+      accessType: '',
+      allowChange: false,
+      record: {},
       rules: {
-        username: { required: true },
+        username: [
+          { required: true, message: '请输入用户名' },
+        ],
+        tags: [
+          { required: true, message: '请输入备注' },
+        ],
         password: [{
           required: true,
+          message: '请输入密码',
+          trigger: ['blur', 'change'],
         }, {
           min: 3,
           max: 32,
           message: '密码长度不符',
+          trigger: ['blur', 'change'],
         }],
+        newPassword: [{
+          required: true,
+          message: '请输入新密码',
+          trigger: ['blur', 'change'],
+        }, {
+          min: 3,
+          max: 32,
+          message: '密码长度不符',
+          trigger: ['blur', 'change'],
+        }],
+        repeatPassword: [
+          { required: true, message: '请再次输入密码' },
+          { validator: validatePass, trigger: ['blur', 'change'] },
+        ],
       },
     }
   },
@@ -84,8 +148,74 @@ export default {
     async loadData() {
       this.tableData = await loadUser()
     },
-    createUsers() {
+    showDialog(type, item) {
+      this.record = {}
+      this.accessType = 'create'
+      if (type === 'edit') {
+        this.record = item
+        this.accessType = 'edit'
+      }
       this.dialogVisible = true
+    },
+    closeDialog() {
+      this.dialogVisible = false
+      this.allowChange = false
+    },
+    togglePassword() {
+      this.allowChange = !this.allowChange
+      // 取消修改密码，删除密码数据
+      if (!this.allowChange) {
+        this.record = {
+          tags: this.record.tags,
+          username: this.record.username,
+        }
+      }
+    },
+    save() {
+      const vue = this
+      this.$refs.recordForm.validate(function(valid) {
+        if (!valid) {
+          return false
+        }
+        if (vue.accessType === 'edit') {
+          const { username } = vue.record
+          updateUser(username, vue.record).then(() => {
+            if (vue.allowChange) {
+              const passwordData = {
+                new_pwd: vue.record.newPassword,
+                old_pwd: vue.record.password,
+              }
+              changePassword(username, passwordData)
+            }
+            vue.$message.success('编辑成功')
+            vue.dialogVisible = false
+            vue.allowChange = false
+            vue.accessType = ''
+            vue.record = {}
+            vue.loadData()
+          })
+        } else {
+          createUser(vue.record).then(() => {
+            vue.$message.success('创建用户成功')
+            vue.dialogVisible = false
+            vue.accessType = ''
+            vue.record = {}
+            vue.loadData()
+          })
+        }
+      })
+    },
+    deleteConfirm(item) {
+      const vue = this
+      this.$confirm('确定删除该用户?', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(() => {
+        destroyUser(item.username).then(() => {
+          vue.$message.success('删除成功')
+          vue.loadData()
+        })
+      })
     },
   },
 }
