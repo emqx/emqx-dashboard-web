335fc37f5f0bf934ada56e22c5a08ec698efd733
tianxl <1823871721@qq.com>
2019-07-04 10:02:23 +0800

    feat(app): 应用列表页完成

--- src/views/Function/Application.vue
+++ src/views/Function/Application.vue
@@ -44,33 +44,103 @@
         class="emq-list-card"
       >
         <div class="emq-table-header">
-          <el-button type="primary" icon="el-icon-plus" size="small" @click="createApplication">创 建</el-button>
+          <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-plus"
+          @click="showDialog('create')">创 建</el-button>
         </div>
 
 
         <el-table :data="tableData" class="data-list">
-          <el-table-column label="跟老的一致" prop="name"></el-table-column>
+          <el-table-column label="应用ID" prop="app_id"></el-table-column>
+          <el-table-column label="应用名称" prop="name"></el-table-column>
+          <el-table-column label="到期时间" prop="expired"></el-table-column>
+          <el-table-column label="备注" prop="desc"></el-table-column>
+          <el-table-column label="状态">
+            <template slot-scope="{ row }">
+              <el-switch
+                v-model="row.status"
+                active-color="#13ce66"
+                inactive-color="#d0d3e0"
+                @change="updateApplication(row)">
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column>
+            <template slot-scope="{ row }">
+              <el-button type="dashed" size="mini" @click="showDialog('view', row)">查看</el-button>
+              <el-button type="dashed" size="mini" @click="showDialog('edit', row)">编辑</el-button>
+              <el-button
+                type="dashed"
+                size="mini"
+                @click="deleteConfirm(row)">删除</el-button>
+            </template>
+          </el-table-column>
         </el-table>
 
 
       </a-card>
     </div>
 
-
-    <el-dialog title="创建应用 或者 编辑应用 你自己维护状态" width="520px" :visible.sync="dialogVisible">
-      <el-form :model="record" :rules="rules" size="small">
-        <el-row :gutter="40">
+    <el-dialog
+      width="600px"
+      :title="accessType === 'edit' ? '编辑应用' : '创建应用'" 
+      :visible.sync="dialogVisible">
+      <el-form size="small" ref="recordForm" :model="record" :rules="rules">
+        <el-row :gutter="20">
           <el-col :span="12">
-            <el-form-item prop="name" label="应用名称">
-              <el-input v-model="record.name"></el-input>
+            <el-form-item prop="app_id" label="应用ID" >
+              <el-input
+                v-model="record.app_id"
+                :disabled="accessType !== 'create'"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col v-if="accessType === 'view'" :span="12">
+            <el-form-item prop="secret" label="密钥" >
+              <el-input v-model="record.secret" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item prop="name" label="应用名称" >
+              <el-input
+                v-model="record.name"
+                :disabled="accessType !== 'create'"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item prop="status" label="状态" >
+              <el-select
+                v-model="record.status"
+                :disabled="accessType === 'view'">
+                <el-option label="启用" :value="true"></el-option>
+                <el-option label="不启用" :value="false"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item prop="expired" label="到期时间" >
+              <el-date-picker
+                v-model="record.expired"
+                type="date"
+                value-format="yyyy-MM-dd"
+                :disabled="accessType === 'view'">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item prop="desc" label="备注" >
+              <el-input
+                v-model="record.desc"
+                :disabled="accessType === 'view'"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
 
-      <div slot="footer" class="dialog-align-footer">
-        <el-button type="primary" size="small">确 定</el-button>
-        <el-button plain size="small">取 消</el-button>
+      <div v-if="accessType !== 'view'" slot="footer" class="dialog-align-footer">
+        <el-button type="primary" size="small" @click="save">确定</el-button>
+        <el-button plain size="small" @click="dialogVisible = false">取消</el-button>
       </div>
     </el-dialog>
   </div>
@@ -81,7 +151,7 @@
 // API 自行去老 Dashboard 抓包
 import {
   loadApp,
-  crateApp,
+  createApp,
   showApp,
   updateApp,
   destroyAPP,
@@ -102,6 +172,7 @@ export default {
       },
       dialogVisible: false,
       tableData: [],
+      accessType: '',
       record: {
         name: 'wivwiv', // 应用名称
         app_id: '7a9984c29896b', // 应用 ID
@@ -111,8 +182,9 @@ export default {
         desc: '', // 描述
       },
       rules: {
-        name: { required: true },
-        status: [{ required: true }, { type: 'enum', enum: [0, 1] }],
+        name: [ { required: true, message: '请输入应用名称' } ],
+        app_id: [ { required: true, message: '请输入应用 ID' } ],
+        status: [ { required: true, message: '请选择是否启用' } ],
       },
     }
   },
@@ -125,8 +197,63 @@ export default {
     async loadData() {
       this.tableData = await loadApp()
     },
-    createApplication() {
+    // 请求一组数据
+    async loadAppData(id) {
+      this.record = await showApp(id)
+    },
+    showDialog(type, item) {
+      this.record = {}
+      this.accessType = type
+      if (type === 'edit') {
+        this.record = item
+      }
+      if (type === 'view') {
+        this.loadAppData(item.app_id)
+      }
       this.dialogVisible = true
+    },
+    updateApplication(item) {
+      updateApp(item.app_id, item).then(() => {
+        this.$message.success('编辑成功')
+      })
+    },
+    save() {
+      const vue = this
+      this.$refs.recordForm.validate(function(valid) {
+        if (!valid) {
+          return false
+        }
+        if (vue.accessType === 'edit') {
+          const { app_id } = vue.record
+          updateApp(app_id, vue.record).then(() => {
+            vue.$message.success('编辑成功')
+            vue.dialogVisible = false
+            vue.accessType = ''
+            vue.record = {}
+            vue.loadData()
+          })
+        } else {
+          createApp(vue.record).then(() => {
+            vue.$message.success('创建应用成功')
+            vue.dialogVisible = false
+            vue.accessType = ''
+            vue.record = {}
+            vue.loadData()
+          })
+        }
+      })
+    },
+    deleteConfirm(item) {
+      const vue = this
+      this.$confirm('确定删除该应用?', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(() => {
+        destroyAPP(item.app_id).then(() => {
+          vue.$message.success('删除成功')
+          vue.loadData()
+        })
+      })
     },
   },
 }
@@ -147,6 +274,12 @@ export default {
       margin-right: 8px;
       font-size: 12px;
     }
+  }
+  .el-select {
+    width: 100%;
+  }
+  .el-date-editor {
+    width: 100%;
   }
 }
 </style>
