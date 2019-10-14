c23ab476741f25dd52a553edf30360dacc34fed0
YuShifan <894402575bt@gmail.com>
2019-10-14 17:58:47 +0800

    Revert "feat(connections): add select node"
    This reverts commit 3bcfe9b19aedb5d686f055497cc58c8643429a5b.

--- src/api/connections.js
+++ src/api/connections.js
@@ -1,34 +1,21 @@
 import http from '@/common/http'
 
-// 获取连接
 export function listConnections(params = {}) {
   return http.get('/connections', { params })
 }
 
-// 获取单个节点下的连接
-export function listNodeConnections(nodeName, params = {}) {
-  return http.get(`/nodes/${nodeName}/connections`, { params })
-}
-
-// 搜索连接
 export function searchConnections(clientId = '') {
   return http.get(`/connections/${encodeURIComponent(clientId)}`)
 }
 
-// 搜索单个节点的连接
-export function searchNodeConnections(nodeName, clientId = '') {
-  return http.get(`/nodes/${nodeName}/connections/${encodeURIComponent(clientId)}`)
-}
-
 // export function banConnection(clientId) {}
 
-// 断开连接
 export function disconnectConnection(clientId = '') {
-  return http.delete(`/connections/${clientId}`)
+  return http.delete(`/connections/${encodeURIComponent(clientId)}`)
 }
 
-// 获取连接的基础信息
 export async function loadConnectionDetail(clientId) {
+  // 基础信息
   const data = await http.get(`/connections/${encodeURIComponent(clientId)}`)
   return data.length ? data[0] || {} : {}
 }
