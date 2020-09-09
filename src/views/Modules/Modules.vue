1841ccad347b026d7042e254c82ab6022266817d
Betty <1942433767@qq.com>
2020-09-09 17:01:30 +0800

    fix(link): fix link and empty value

--- src/views/Modules/Modules.vue
+++ src/views/Modules/Modules.vue
@@ -169,7 +169,8 @@ export default {
       this.showList = this.list
     },
     toReadMore() {
-      window.open('https://docs.emqx.net')
+      const windowUrl = window.open('https://docs.emqx.net')
+      windowUrl.opener = null
     },
   },
 }
