466dc41c381e83895128d7a6eab903e4e1630fae
Kerry <yueming.sheng@emqx.io>
2021-04-29 10:31:55 +0800

    refactor(eslint, prettier): update eslint and its peripheral assets

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,16 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: ['prettier'],
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/prettier',
+    'plugin:vue/strongly-recommended',
+    'prettier',
   ],
   rules: {
     'prettier/prettier': 'error',
     'no-console': 'off',
     'no-debugger': 'off',
     'import/extensions': 0,
-    'vue/max-attributes-per-line': [
-      4,
-      {
-        singleline: 6,
-        multiline: {
-          max: 1,
-          allowFirstLine: false,
-        },
-      },
-    ],
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/name-property-casing': ['error', 'PascalCase'],
     'vue/no-v-html': 'off',
     semi: 0,
     'import/no-cycle': 0,
@@ -33,7 +20,6 @@ module.exports = {
     'no-underscore-dangle': 0,
     'prefer-destructuring': ['error', { object: true, array: false }],
     'no-param-reassign': ['error', { props: false }],
-    ignoreDestructuring: true,
     camelcase: 'off',
   },
   parserOptions: {
