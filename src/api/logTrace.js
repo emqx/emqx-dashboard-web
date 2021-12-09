c901b5da756344f94c26d000e91291198754c0cf
Kinplemelon <wujiu529@gmail.com>
2021-12-09 11:20:43 +0800

    fix(log trace): fixed some bugs in log trace

--- src/api/logTrace.js
+++ src/api/logTrace.js
@@ -1,5 +1,6 @@
 import http from '@/common/http'
 import store from '@/stores'
+import { createURLWithAuth } from '../common/utils'
 
 export function getTraceList() {
   return http.get('/trace')
@@ -16,14 +17,14 @@ export function getTraceLog(name, params) {
 
 export function downloadTrace(name) {
   const link = document.createElement('a')
-  link.href = `${store.getters.httpBaseUrl}/trace/${encodeURIComponent(name)}/download`
+  link.href = createURLWithAuth(`${store.getters.httpBaseUrl}/trace/${encodeURIComponent(name)}/download`)
   // link.setAttribute('download', 'emqx.zip')
   document.body.appendChild(link)
   link.click()
   return Promise.resolve()
 }
 export function stopTrace(name) {
-  return http.put(`/trace/${encodeURIComponent(name)}/disable`)
+  return http.put(`/trace/${encodeURIComponent(name)}/stop`)
 }
 
 export function deleteTrace(name) {
