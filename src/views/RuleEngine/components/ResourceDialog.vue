0d01322962b2fa1873fc2b3f9736c72292f391d1
Kinplemelon <wujiu529@gmail.com>
2022-05-13 17:22:02 +0800

    feat(resource): support config selece in resource

--- src/views/RuleEngine/components/ResourceDialog.vue
+++ src/views/RuleEngine/components/ResourceDialog.vue
@@ -69,6 +69,17 @@
               <template v-if="item.elType === 'object'">
                 <key-and-value-editor v-model="record.config[item.key]"></key-and-value-editor>
               </template>
+              <!-- config select -->
+              <template v-else-if="item.elType === 'cfgselect'">
+                <config-select
+                  v-model="record.config[item.key]"
+                  v-bind="item.bindAttributes"
+                  class="reset-width"
+                  :extraConfigs="item.extraConfigs"
+                  @updateConfig="addConfigAccordingType"
+                >
+                </config-select>
+              </template>
               <!-- input -->
               <template v-else-if="item.elType !== 'select'">
                 <el-input
@@ -145,11 +156,12 @@ import { renderParamsForm, verifyID } from '@/common/utils'
 import KeyAndValueEditor from '@/components/KeyAndValueEditor'
 import FileEditor from '@/components/FileEditor'
 import _ from 'lodash'
+import ConfigSelect from '@/components/ConfigSelect'
 
 export default {
   name: 'ResourceDialog',
 
-  components: { KeyAndValueEditor, FileEditor },
+  components: { KeyAndValueEditor, FileEditor, ConfigSelect },
 
   inheritAttrs: false,
 
@@ -178,6 +190,13 @@ export default {
       resourceTypes: [],
       configList: [],
       wholeConfigList: [],
+      originConfigList: [],
+      originRules: {
+        config: {},
+      },
+      originRecord: {
+        config: {},
+      },
       types: [],
       selectedResource: {
         name: '',
@@ -277,6 +296,7 @@ export default {
       this.wholeConfigList = form
       this.showMoreItem = false
       this.configList = this.wholeConfigList
+      this.storeOriginData({ form, rules })
 
       if (this.oper === 'edit') {
         this.assignValuesToRecord()
@@ -313,6 +333,70 @@ export default {
         const value = configVal[key]
         this.$set(this.record.config, key, value)
       })
+    },
+    storeOriginData(configData) {
+      const { form, rules } = configData
+      this.originConfigList = form
+      this.originRules.config = rules
+      this.originRecord.config = {}
+      form.forEach(({ key, value }) => {
+        this.$set(this.originRecord.config, key, value)
+      })
+    },
+    /**
+     * find out which fields need to be added and which fields need to be deleted
+     * @param {Array<ConfigItem>} oldConfig
+     * @param {Array<ConfigItem>} newConfig
+     * @returns {needAdded:Array<string>,needDeleted:Array<string>}
+     */
+    diffConfigList(oldConfig, newConfig) {
+      const oldKeys = oldConfig.map(({ key }) => key)
+      const newKeys = newConfig.map(({ key }) => key)
+      const needDeleted = oldKeys.filter((oldItem) => !newKeys.some((newItem) => newItem === oldItem))
+      const needAdded = newKeys.filter((newItem) => !oldKeys.some((oldItem) => oldItem === newItem))
+      return {
+        needDeleted,
+        needAdded,
+      }
+    },
+    addConfigAccordingType(extraConfigs, type, totalExtraConfigs, isInit = false) {
+      const [...commonConfig] = this.originConfigList
+      const { ...rulesCommonConfig } = this.originRules.config
+      const { ...recordCommonConfig } = this.originRecord.config
+      const { needDeleted: needDeletedConfigs, needAdded: needAddedConfigs } = this.diffConfigList(
+        this.configList,
+        commonConfig,
+      )
+      const record = {
+        ..._.omit(this.record.config, needDeletedConfigs),
+        ..._.pick(recordCommonConfig, needAddedConfigs),
+      }
+
+      if (Object.keys(extraConfigs).length) {
+        const configData = renderParamsForm(extraConfigs, 'config')
+        const { form, rules } = configData
+        const addConfigs = {}
+        if (isInit && this.oper === 'edit') {
+          form.forEach(({ key, value }) => {
+            this.$set(addConfigs, key, this.record.config[key] || value)
+          })
+        } else {
+          form.forEach(({ key, value }) => {
+            this.$set(addConfigs, key, value)
+          })
+        }
+        this.configList = commonConfig.concat(form)
+        this.rules.config = Object.assign(rulesCommonConfig, rules)
+        this.record.config = Object.assign(record, addConfigs)
+      } else {
+        this.configList = commonConfig
+        this.rules.config = rulesCommonConfig
+        this.record.config = record
+      }
+      this.record.config.type = type
+      if (this.$refs.record) {
+        setTimeout(this.$refs.record.clearValidate, 10)
+      }
     },
 
     async handleEdit(record) {
