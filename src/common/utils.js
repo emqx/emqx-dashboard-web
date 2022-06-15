ce4563ab27cdaa64e1b1e4abe777affd1b4f1cd6
Kinplemelon <wujiu529@gmail.com>
2022-06-15 15:12:19 +0800

    feat(rule): supported editable_select

--- src/common/utils.js
+++ src/common/utils.js
@@ -197,6 +197,7 @@ export function renderParamsForm(params = {}, propPrefix = '') {
     }
     const inputPlaceholder = isDescTooShort(description) && propPrefix !== 'configs' ? description : ''
     // 表单类型, 渲染使用的属性
+    const isEditableSelect = input === 'editable_select'
     form.push({
       formItemAttributes: {
         prop: propPrefix ? `${propPrefix}.${k}` : k,
@@ -211,6 +212,8 @@ export function renderParamsForm(params = {}, propPrefix = '') {
         field: elType === 'select' || elType === 'cfgselect' ? field : undefined,
         placeholder: inputPlaceholder,
         rows: inputType === 'textarea' ? 5 : 0,
+        filterable: isEditableSelect,
+        'allow-create': isEditableSelect,
       },
       key: k,
       type: inputType,
@@ -233,7 +236,7 @@ export function renderParamsForm(params = {}, propPrefix = '') {
         rules[k].push({ required: true, message: elType === 'input' ? requiredInputText : requiredSelectText })
       }
     }
-    if (enumValue) {
+    if (enumValue && !isEditableSelect) {
       const options = enumValue.map(($) => (typeof $ === 'boolean' ? $.toString() : $))
       rules[k].push({ type: 'enum', enum: options })
     }
