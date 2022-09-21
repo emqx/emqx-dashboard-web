d819f14fad7eb4d7673bf47110b5cd74e3afa1f9
Kinplemelon <wujiu529@gmail.com>
2022-09-21 14:56:22 +0800

    refactor(layout): pop up message to remind user change default pwd firstly

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -119,9 +119,10 @@ export default {
   },
 
   async created() {
-    await this.initLicense()
-    if (!this.isEvaluationLicense && this.isUsingDefaultPwd) {
+    if (this.isUsingDefaultPwd) {
       this.popupMessageBox()
+    } else if (this.isEvaluationLicense) {
+      this.initLicense()
     }
   },
 }
